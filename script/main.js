fb045e5d32b39061936ca4f1e6691ef63b455b90
amdog <2199623418@qq.com>
2021-05-22 16:01:38 +0800

    reset

--- script/main.js
+++ script/main.js
@@ -1,116 +1,79 @@
-window.onload = function () {
-    const clamp = (x, min, max) => Math.max(min, Math.min(x, max));
-    let start = new Date().getTime()
-    window.onscroll = (e) => {
-        const cards = Array.from(document.querySelectorAll(".card"));
-        cards.forEach((card, i) => {
-            //节流
-            if (document.documentElement.clientHeight + e.target.documentElement.scrollTop >= document.documentElement.offsetHeight-600) {
-                if (new Date().getTime() > start + 1000 && now > 0) {
-                    let tmp = now + 1;
-                    now = -9999999999999;
-                    ajax(`https://amdog.github.io/page/${tmp}.html`, (data) => {
-                        if (!!data) {
-                            now = tmp
-                            let regex = /.*?<title>(.*?)<\/title>.*?/
-                            createEle(tmp)
-                            let e = document.getElementById(`t${tmp}`)
-                            e.innerHTML = regex.exec(data)[0];
-                            e.innerHTML = e.textContent;
-                            isMobile()
-                        }
-                    })
-                    start = new Date().getTime()
-                }
-            }
-            resizeZ(card,i,e)
-        });
-    }
-
-    function resizeZ(card,i,e){
-            const scale = clamp((400 + 300 * i - e.target.documentElement.scrollTop) / 300, 0, 1);
-            const opacity = clamp((400 + 300 * i - e.target.documentElement.scrollTop) / 300, 0, 1);
-            card.style.transform = `scale(${scale})`;
-            card.style.opacity = `${opacity}`;
-            card.style.zIndex = `${i}`
-    }
-
-
-    setTimeout(() => {
-        for (let i = 1; i <= 5; i++) {
-            ajax(`https://amdog.github.io/page/${i}.html`, (data) => {
-                if (!!data) {
-                    let regex = /.*?<title>(.*?)<\/title>.*?/
-                    createEle(i)
-                    let e = document.getElementById(`t${i}`)
-                    e.innerHTML = regex.exec(data)[0];
-                    e.innerHTML = e.textContent
-                }
-            })
-        }
-    }, 500);
-}
-
-let now = 5
-
-document.getElementsByTagName('input')[1].onclick=function searchAc() {
-    let keyword = document.getElementsByTagName('input')[0].value
-    window.location.href='https://github.com/amdog/amdog.github.io/search?q='+keyword
-}
-
-
-function createEle(index) {
-    let div = document.createElement('div')
+function appendEle(index) {
+    var div = document.createElement('div')
     div.className = 'card';
     div.id = `c${index}`
-    div.innerHTML = `<div class="dsc">
-    <img src="./img/date.svg" alt="">${parseInt(Math.random()*12%12)}-${parseInt(Math.random()*28%28)}
-    <img src="./img/eay.svg" alt="">${parseInt(Math.random()*1000)}
-</div>
-<div class="main">
-    <div class="ifra">
-        <iframe  scrolling='no' id='i${index}' src='https://amdog.github.io/page/${index}.html' frameborder="0"></iframe>
-    </div>
-    <a href='https://amdog.github.io/page/${index}.html'> <div class="title" id='t${index}'></div></a>
-</div>`
-    let list = document.getElementsByClassName('list')[0]
-    list.appendChild(div);
-    isMobile()
+    div.innerHTML = `<iframe scrolling='no' id='i${index}' onload='joinTitle(this,${index})'  src='https://amdog.github.io/page/${index}.html' frameborder="0"></iframe><span class="title" id='b${index}' ><a href='https://amdog.github.io/page/${index}.html' id='t${index}' ></a></span>`
+    var box = document.getElementsByClassName('card-box')[index - 1]
+    box.appendChild(div)
 }
 
-function ajax(url, cb) {
-    let xmlhttp;
+var i = 0
+
+window.onload = function() {
+    for (var b = 0; b < 10; b++) {
+        createBox()
+    }
+    var clock = setInterval(function() {
+        i++
+        appendEle(i)
+        if (i >= 10) {
+            clearInterval(clock)
+        }
+    }, 100)
+}
+
+
+var bgList = ['#fda34b', '#FD84AD', '#009688', '#e95c5a', '#86ba4b', '#86C1B9']
+
+function joinTitle(f, i) {
+    var title = f.contentWindow.document.title
+    document.getElementById(`t${i}`).innerText = title
+    document.getElementById(`b${i}`).style.background = bgList[document.getElementsByClassName('card').length % 6]
+    var li = document.createElement('li')
+    li.innerText = title
+}
+
+
+function createBox() {
+    var box = document.createElement('div')
+    box.className = 'card-box'
+    var list = document.getElementsByClassName('list')[0]
+    list.appendChild(box)
+}
+
+function ping(url, cb) {
+    var xmlhttp
     if (window.XMLHttpRequest) {
         xmlhttp = new XMLHttpRequest();
     } else {
         xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
     }
-    xmlhttp.onreadystatechange = function () {
+    xmlhttp.onreadystatechange = function() {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            cb(xmlhttp.responseText);
+            cb(1)
         } else {
-            cb(null);
+            cb(0)
         }
     }
     xmlhttp.open("GET", url, true);
     xmlhttp.send();
 }
 
-
-function isMobile(){
-    if(document.documentElement.clientHeight > document.documentElement.clientWidth){
-        document.getElementsByClassName('list')[0].style.width='100%'
-        document.getElementsByClassName('sear-box')[0].style.width='220px'
-        document.getElementsByTagName('input')[0].style.width='140px'
-        document.getElementsByTagName('input')[1].style.width='50px'
-        document.documentElement.style.fontSize='5px'
-        document.querySelectorAll('.card').forEach(c=>{
-            c.style.width='100%'
+window.onscroll = function() {
+    var visionHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight
+    var scrolledHeight = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop
+    var trueHeight = document.documentElement.scrollHeight || document.body.scrollHeight
+    if (trueHeight == visionHeight + scrolledHeight) {
+        ping(`https://amdog.github.io/page/${i+1}.html`, function(t) {
+            if (t == 1) {
+                i++
+                if (i <= document.getElementsByClassName('card-box').length) {
+                    for (var b = 0; b < 4; b++) {
+                        createBox()
+                    }
+                }
+                appendEle(i)
+            }
         })
-        document.querySelectorAll('.ifra').forEach(c=>{
-            c.style.width='280px'
-            c.style.height='200px'
-        })
-
     }
 }
